c027cfed1c56c80bbd82aa2b7e20bf5339fb680f
iqra30 <51120066+iqra30@users.noreply.github.com>
2019-07-26 02:01:32 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,140 @@
+let videos = [
+    {
+      title: "Photoshop tutorial",
+      lengthInMinutes: 70,
+      category: "Education",
+      uploadDate: new Date("07-22-2019"),
+      tags: "design, digital, photoshop, creativity",
+      features: ["Live", "360°", "HDR"],
+      viewCount: 4700,
+      rating: 4.3
+    },
+    {
+      title: "Episode # 01 - The Best Comedy Show",
+      lengthInMinutes: 19,
+      category: "Entertainment",
+      uploadDate: new Date("07-03-2019"),
+      tags: "comedy, funny",
+      features: ["Subtitles/CC", "3D", "HD"],
+      viewCount: 145615,
+      rating: 3.9
+    },
+    {
+      title: "How to use FOR EACH loop - tutorial by Tech Karo",
+      lengthInMinutes: 6,
+      category: "Education",
+      uploadDate: new Date("11-10-2018"),
+      tags: "javascript, loops, web development",
+      features: ["Purchased", "HD"],
+      viewCount: 9004,
+      rating: 4.5
+    },
+    {
+        title: "Vlog - trip to hunza",
+        lengthInMinutes: 12,
+        category: "Entertainment",
+        uploadDate: new Date("15-07-2007"),
+        tags: "travel",
+        features: ["1080hd", "HD"],
+        viewCount: 110010,
+        rating: 4.1
+      },
+      {
+        title: "Marvel studio - endgame trailer",
+        lengthInMinutes: 3,
+        category: "Entertainment",
+        uploadDate: new Date("24-04-2019"),
+        tags: "Super Heroes, thanos",
+        features: ["Purchased", "HD"],
+        viewCount: 70786543,
+        rating: 4.9
+      },
+      {
+        title: "How to learn ABC from different pictures",
+        lengthInMinutes: 17,
+        category: "Education",
+        uploadDate: new Date("1-11-2012"),
+        tags: "Childrens , easylearning",
+        features: ["Auto", "HD"],
+        viewCount: 4604,
+        rating: 3.2
+      },
+      {
+        title: "Dil Dil Pakistan",
+        lengthInMinutes: 5,
+        category: "Music",
+        uploadDate: new Date("14-07-1998"),
+        tags: "Patrotic, Country love",
+        features: ["Purchased"],
+        viewCount: 5678004,
+        rating: 4.9
+      },
+      {
+        title: "Official Trailer - transformer",
+        lengthInMinutes: 2,
+        category: "Movie",
+        uploadDate: new Date("19-09-2017"),
+        tags: "robots, fiction",
+        features: ["Auto", "HD"],
+        viewCount: 9005694,
+        rating: 4.5
+      },
+      {
+        title: "How to cook spagetti",
+        lengthInMinutes: 10,
+        category: "Blog",
+        uploadDate: new Date("11-12-2008"),
+        tags: "cooking",
+        features: ["1080hd", "HD"],
+        viewCount: 90084,
+        rating: 3.5
+      },
+      {
+        title: "how to overcome your depression",
+        lengthInMinutes: 25,
+        category: "People and blog",
+        uploadDate: new Date("21-06-2018"),
+        tags: "motivation",
+        features: ["Purchased", "auto", "HD"],
+        viewCount: 905604,
+        rating: 4.1
+      }
+  ];
+
+// for (let i = 0; i < videos.length; i++) {
+//     console.log("title : " + videos[i].title)
+//     console.log("Length : " + videos[i].lengthInMinutes)
+//     console.log("Catagory : "+ videos[i].tags)
+//     console.log("Views : " + videos[i].viewCount)
+//     console.log("Uploaded On : " + videos[i].uploadDate)
+//     console.log("Rating : " + videos[i].rating);
+    
+// }
+
+// for( let i = 0; i < videos.length; i++){
+//   if(videos[i].lengthInMinutes < 3){
+//     console.log(videos[i].title)
+//   }
+// }
+
+
+// for( let i = 0; i < videos.length; i++){
+//   if(videos[i].lengthInMinutes > 20){
+//      console.log( videos[i].title)
+//     console.log(videos[i].lengthInMinutes)
+//   }
+//   }
+
+
+// for( let i = 0; i < videos.length; i++){
+//   if(videos[i].lengthInMinutes >= 7){
+//     console.log(videos[i].lengthInMinutes)
+//   }
+// }
+
+// for( let i = 0; i < videos.length; i++){
+//   if(videos[i].lengthInMinutes === 70){
+//     console.log(videos[i].title)
+//     console.log(videos[i].lengthInMinutes)
+//   }
+// }
